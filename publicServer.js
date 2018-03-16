5e98916714c1f297c788b0db0c6e0b8a405a9d51
Bojan Dragojevic <bdragojevic@gmail.com>
2018-03-16 14:31:19 +0100

    built production fix

--- publicServer.js
+++ publicServer.js
@@ -2,7 +2,6 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import compression from 'compression';
-import favicon from 'serve-favicon';
 
 /*eslint-disable no-console */
 
@@ -10,8 +9,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(compression());
-app.use(express.static('public'));
-app.use(favicon(path.join(__dirname,'assets','public','favicon.ico')));
+app.use(express.static('dist'));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, './dist/index.html'));
